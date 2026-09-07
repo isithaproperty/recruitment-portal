a7feeda33298d078ba5fa71d7baff5c515d22340
isithaproperty <brian.klue@isitha.global>
2026-09-07 22:37:35 +0200

    Protect recruiter routes through the root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import CvReviewDecisionAlerts from "./CvReviewDecisionAlerts";
+import RecruiterAuthGuard from "./components/RecruiterAuthGuard";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,8 +30,10 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
-        {children}
-        <CvReviewDecisionAlerts />
+        <RecruiterAuthGuard>
+          {children}
+          <CvReviewDecisionAlerts />
+        </RecruiterAuthGuard>
       </body>
     </html>
   );
